f5730524549a45767e5bc513e3d5b99e0993266a
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-04-10 12:25:17 +0530

    Added more code in the file of PHP.

--- phpCode.php
+++ phpCode.php
@@ -1,6 +1,77 @@
 <?php
+
+
+$string ="MY NAME IS GYANENDRA YADAV";
+//echo strlen($string);
+//echo str_word_count($string);
+//echo strrev($string);
+//echo strpos("$string","YADAV");
+$string=str_replace("MY","I","$string");
+echo str_replace("NAME","AM","$string");
+//echo ("$string");
+
+
+
+// int [] ar= new int[size];
+//int []arr={"gya","GDUD"};
+//$arr= array("ram","shyam");
+//var_dump($arr);
+/*
+$num=1323;
+$name="Gyan";
+$x = 10.365;
+var_dump($x)."<br>";
+var_dump($num)."<br>";
+var_dump($name)."<br>";
+*/
+
+/*
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP at " . $txt2 . "<br>";
+print $x + $y;
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * // o/p>> My name is Sejal Bansal
+ $var="Sejal";
+echo " My name is $var Bansal"."<br>";
+echo "My Name is ".$var." Bansal";
+// System.out.println("Hello"+______);
+ */
+
+/*
     $a=0;
     $a=5>2? print("4"):3;
     print($a);
+    */
 ?>
 
