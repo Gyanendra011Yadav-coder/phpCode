3c4348457e87a69782ef7c6482c43403eb212a38
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-04-10 21:23:40 +0530

    Worte the code to create a Chess board using PHP.

--- chessBoardPrint.php
+++ chessBoardPrint.php
@@ -1,4 +1,15 @@
 <?php
-
-
+echo "<table border='2px black' style='border-bottom-style: double' >";
+for ($row=0;$row<8;$row++){
+    echo "<tr>";
+    for($column=0;$column<8;$column++){
+        if(($row+$column)%2==0){
+            echo"<td height='25 px' width='25 px' bgcolor='black'> </td>";
+        }else{
+            echo "<td height='25px' width='25px' bgcolor='white'></td>";
+        }
+    }
+    echo "</tr>";
+}
+echo "</table>"
 ?>
