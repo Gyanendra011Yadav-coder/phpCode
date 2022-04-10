2e1bf5d6b61073c4d43bad097fb9eed2bb5af659
Gyanendra Yadav <gyanendra1018yadav@gmail.com>
2022-04-10 21:09:05 +0530

    added the piece of code solving further numerical value.

--- chessBoardPrint.php
+++ chessBoardPrint.php
@@ -1,10 +1,4 @@
 <?php
-echo "<table>";
-    for($row=1;$row<=8;$row++){
-        for($col=1;$col<=8;$col++){
 
-        }
-
-    }
 
 ?>
--- phpCode.php
+++ phpCode.php
@@ -1,5 +1,12 @@
 <?php
-e
+
+$m=null;
+$n="50";
+if($m??$n){
+    printf("%o",$m=11);
+}else{
+    echo $n;
+}
 
 
 /*
